b0bdb550babfced0b65cd3d41c677eb8544268a5
Shubhojit Bhattacharjee <shubhojit.bhattacharjee@thoughtfocus.com>
2020-03-28 14:58:27 +0530

    Till Email labeled Social and Sent

--- src/test/java/com/crossover/e2e/TestGmail.java
+++ src/test/java/com/crossover/e2e/TestGmail.java
@@ -1,14 +1,16 @@
 package com.crossover.e2e;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import util.CommonUtil;
 
 import java.io.File;
 import java.io.FileReader;
@@ -22,27 +24,15 @@ public class TestGmail {
     @Before
     public void setUp() throws Exception {
 
-//        GmailQuickstart.authorizeGmail();
-
         properties.load(new FileReader(new File("src/test/resources/test.properties")));
         //Dont Change below line. Set this value in test.properties file incase you need to change it..
-        System.setProperty("webdriver.chrome.driver",properties.getProperty("webdriver.chrome.driver") );
-
-//        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-web-security");
-        options.addArguments("--allow-running-insecure-content");
-//        capabilities.setCapability("chrome.binary",
-//                properties.getProperty("webdriver.chrome.driver"));
-//        capabilities.setCapability( ChromeOptions.CAPABILITY, options);
-
-        driver = new ChromeDriver(options);
-
-//        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", properties.getProperty("webdriver.chrome.driver") );
+        driver = new ChromeDriver();
     }
 
+    @After
     public void tearDown() {
-        driver.quit();
+//        driver.quit();
     }
 
     /*
@@ -63,7 +53,7 @@ public class TestGmail {
 
         Thread.sleep( 10000 );
 
-        webDriverWait.until( ExpectedConditions.visibilityOf(driver.findElement( By.name("password"))))
+        webDriverWait.until( ExpectedConditions.elementToBeClickable(driver.findElement( By.name("password"))))
                 .sendKeys( properties.getProperty("password") );
 
         driver.findElement(By.id("passwordNext")).click();
@@ -73,14 +63,26 @@ public class TestGmail {
 
         webDriverWait.until(ExpectedConditions.elementToBeClickable
                 ( By.name("to"))).clear();
-
-//        driver.findElement(By.name("to")).clear();
         driver.findElement(By.name("to")).sendKeys(String.format("%s@gmail.com", properties.getProperty("username")));
-        driver.findElement(By.xpath("//*[@role='button' and text()='Send']")).click();
 
         // emailSubject and emailbody to be used in this unit test.
-        String emailSubject = properties.getProperty("email.subject");
-        String emailBody = properties.getProperty("email.body");
+        String emailSubject = properties.getProperty("email.subject") + CommonUtil.getCurrentSystemDateTime();
+        String emailBody = properties.getProperty("email.body") + CommonUtil.getCurrentSystemDateTime();
 
+        driver.findElement( By.name( "subjectbox" ) ).sendKeys( emailSubject );
+        driver.findElement( By.cssSelector( "[aria-label='Message Body'][role='textbox']" ) ).sendKeys( emailBody );
+
+        driver.findElement( By.cssSelector( "div[data-tooltip='More options'][role='button']" ) ).click();
+
+        WebElement label = webDriverWait.until( ExpectedConditions.elementToBeClickable(
+                By.xpath( "//div[@role='menuitem']/div[contains(text(),'Label')]")));
+
+        Actions actions = new Actions( driver );
+        actions.moveToElement( label ).perform();
+
+        webDriverWait.until( ExpectedConditions.elementToBeClickable(
+                By.cssSelector( "div[title='Social']" ))).click();
+
+        driver.findElement(By.xpath("//*[@role='button' and text()='Send']")).click();
     }
 }
